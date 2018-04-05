be1ee5be4ca1c35747aeb675b041f2b287072dc3
mnfmnfm <mnfmnfm@gmail.com>
2018-04-05 15:18:51 -0700

    simplify soccer balls for code 201

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
 console.log("it's linked")
 
 
-$(document).ready(function() {
-  $(".soccer-ball-pic").on("click", function() {
-    $(this).parent().append(`hello ${$('input').val()}`)
-  });
-
-});
