36e0a72c22bc9ae29930e9fcef4f699f5d1ee56a
mnfmnfm <mnfmnfm@gmail.com>
2018-04-13 12:35:42 -0700

    demo of debugger

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-console.log("it's linked")
+'use strict';
+function clickLogger() {
+  console.log('there was a click!');
+}
+function setUpBalls(event) {
 
+  var balls = document.getElementsByClassName('soccer-ball-pic');
 
+  for (var ball of balls) {
+    debugger
+    ball.addEventListener('click', clickLogger);
+  }
+}
+
+document.getElementsByTagName('h1')[0].addEventListener('click', setUpBalls);
+
+console.log('it\'s linked');
